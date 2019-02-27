4db0e3a2cc48b09162c1bb2dbcf098b3598cd033
iloliloli <iloliloli@qq.com>
2019-02-27 15:39:27 +0800

    20190227

---
+++ files/登录页/data.js
@@ -0,0 +1,7 @@
+﻿$axure.loadCurrentPage(
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,c,d,e,f,g,h,g,i,_(j,k),l,[m],n,_(o,p,q,r,s,t,u,_(),v,_(w,x,y,z,A,_(B,C,D,E),F,null,G,z,H,z,I,J,K,null,L,M,N,O,P,Q,R,M),S,_(),T,_(),U,_(V,[_(W,X,Y,j,Z,ba,q,bb,bc,bb,bd,be,v,_(bf,_(bg,bh,bi,bj),w,bk),S,_(),bl,_(),bm,g)])),bn,_(),bo,_(bp,_(bq,br)));}; 
+var b="url",c="登录页.html",d="generationDate",e=new Date(1551252600017.41),f="isCanvasEnabled",g=false,h="isAdaptiveEnabled",i="sketchKeys",j="",k="s0",l="variables",m="OnLoadVariable",n="page",o="packageId",p="81ffd1ad5987421785d81e0e830a8e6c",q="type",r="Axure:Page",s="name",t="登录页",u="notes",v="style",w="baseStyle",x="627587b6038d43cca051c114ac41ad32",y="pageAlignment",z="near",A="fill",B="fillType",C="solid",D="color",E=0xFFFFFFFF,F="image",G="imageHorizontalAlignment",H="imageVerticalAlignment",I="imageRepeat",J="auto",K="favicon",L="sketchFactor",M="0",N="colorStyle",O="appliedColor",P="fontName",Q="Applied Font",R="borderWidth",S="adaptiveStyles",T="interactionMap",U="diagram",V="objects",W="id",X="71aefa62904b48cdb1017c9cb472bbf3",Y="label",Z="friendlyType",ba="矩形",bb="vectorShape",bc="styleType",bd="visible",be=true,bf="size",bg="width",bh=1024,bi="height",bj=850,bk="4b7bfc596114427989e10bb0b557d0ce",bl="imageOverrides",bm="generateCompound",bn="masters",bo="objectPaths",bp="71aefa62904b48cdb1017c9cb472bbf3",bq="scriptId",br="u1148";
+return _creator();
+})());
